15b056327fe3f481ac6d440a88c2029d20b51c75
Tanner B <sonictanpat@gmail.com>
2025-01-23 19:42:35 -0600

    finished 4

---
+++ script.js
@@ -0,0 +1,25 @@
+//Ex 1
+// const car = {
+//     make: "Toyota",
+//     model: "Prius",
+//     year: "2018",
+//     getInfo: function() { console.log(car);}
+// };
+
+//Ex 3
+function changeColor() {
+    document.getElementById("myDiv").style.backgroundColor = "blue";
+}
+
+//Ex 4
+function changeText() {
+    document.getElementById('myButton').textContent = 'Im changed!';
+}
+
+//Ex 5
+function addList() {
+    
+}
+
+
+
